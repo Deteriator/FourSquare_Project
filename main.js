25e6850cbb288fe5b4a955011f6d10bb7228fc4e
Thomas Hallinger <tomashallinger@gmail.com>
2016-07-07 18:33:55 +0000

    Updated foursquare project

--- main.js
+++ main.js
@@ -1,64 +1,11 @@
-function addContacts (){
-    return {"Name" : X,
-            "Location": Y,
-            "Phone#" : Z
-    }
+$("#submit").click(function(){
+
+
     
-    function searchContacts(contacts, query) {
-    var results = {};
-    for (var key in contacts) {
-        results[key]=contacts[key]
-    }
-    return results;
+
 }
+$("#location").keydown(function(w){
+var x=w.which;
+if(x === 13){
+    var userText=$("#location").val
 
-var restaurants = {};
-
-$(document).ready(function()
-{
-	contacts = addContacts();
-
-	showContacts(contacts);
-	$('#query').focus();
-
-	function showContacts(contacts)
-	{
-		$("#results .contact-row").remove();
-		$("#results").hide();
-
-		for (key in contacts) 
-		{
-			// Create new contact-row
-			if ($("#results .contact-row").length == 0 || $("#results .contact-row").last().children().length == 5)
-			{
-				$("#results").append($('templates .contact-row').clone());
-			}
-	
-			// New column
-			if ($("#results.contact-row").last().children().length < 5)
-			{
-				var contactElement = $('templates .contact').clone();
-				contactElement.find('img').attr('src',contacts[key]["picture"]);
-				contactElement.find('.Name').text(key);
-				contactElement.find('.Location').text(contacts[key]["location"]);
-				contactElement.find('.Phone#').text(contacts[key]["Phone#"]);
-
-				$("#results .contact-row").last().append(contactElement);
-			}
-		}
-
-		$("#results").fadeIn(500);
-	}
-
-	$('#search-btn').click(function()
-	{
-		if ($('#query').val().trim() == "")
-			showContacts(contacts);
-
-		else
-		{
-			var foundContacts = searchContacts(contacts, $('#query').val().trim());
-			showContacts(foundContacts);
-		}
-	});
-});
